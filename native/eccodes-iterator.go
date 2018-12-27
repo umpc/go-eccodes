1c01d4df2f968ef59100b3aaf2e9106e66fc6b2a
Justin Lowery <umpc@users.noreply.github.com>
2018-12-27 14:56:45 -0500

    Update eccodes-iterator.go

--- native/eccodes-iterator.go
+++ native/eccodes-iterator.go
@@ -22,8 +22,18 @@ func Ccodes_grib_iterator_new(handle Ccodes_handle, flags int) (Ccodes_grib_iter
   return ccodes_grib_iterator, nil
 }
 
-func Ccodes_grib_iterator_next(kiter Ccodes_grib_iterator, latitude *C.double, longitude *C.double, value *C.double) int {
-	return int(C.codes_grib_iterator_next((*C.codes_iterator)(kiter), (*C.double)(latitude), (*C.double)(longitude), (*C.double)(value)))
+func Ccodes_grib_iterator_next(kiter Ccodes_grib_iterator) (latitude float64, longitude float64, value float64, err error) {
+	res := int(C.codes_grib_iterator_next(
+		(*C.codes_iterator)(kiter),
+		(*C.double)(unsafe.Pointer(&latitude)),
+		(*C.double)(unsafe.Pointer(&longitude)),
+		(*C.double)(unsafe.Pointer(&value)),
+	))
+	if res != 0 {
+		return 0, 0, 0, errors.New(Cgrib_get_error_message(int(res)))
+	}
+
+	return latitude, longitude, value, nil
 }
 
 func Ccodes_grib_iterator_delete(kiter Ccodes_grib_iterator) error {
