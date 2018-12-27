392ffcc63001bef7258c64741abbc71d2910e4dc
Justin Lowery <umpc@users.noreply.github.com>
2018-12-27 13:25:07 -0500

    Update types.go

--- native/types.go
+++ native/types.go
@@ -5,5 +5,6 @@ import "unsafe"
 type CFILE = unsafe.Pointer
 type Ccodes_handle = unsafe.Pointer
 type Ccodes_keys_iterator = unsafe.Pointer
+type Ccodes_grib_iterator = unsafe.Pointer
 type Ccodes_context = unsafe.Pointer
 type Ccodes_index = unsafe.Pointer
