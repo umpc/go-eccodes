45e26a3adc848f7fa645d1bf71cb0fa5a9076425
Justin Lowery <umpc@users.noreply.github.com>
2018-12-27 15:25:56 -0500

    Update eccodes-iterator.go

--- native/eccodes-iterator.go
+++ native/eccodes-iterator.go
@@ -10,18 +10,16 @@ import (
 	"github.com/amsokol/go-errors"
 )
 
-func Ccodes_grib_iterator_new(handle Ccodes_handle, flags int) (Ccodes_grib_iterator, error) {
-  var err C.int
-
-  ccodes_grib_iterator := unsafe.Pointer(C.codes_grib_iterator_new((*C.codes_handle)(handle), C.ulong(Culong(flags)), (*C.int)(&err)))
+func Ccodes_grib_iterator_new(handle Ccodes_handle, flags int) (Ccodes_iterator, error) {
+  err := C.int(0)
+  ccodes_iterator := unsafe.Pointer(C.codes_grib_iterator_new((*C.codes_handle)(handle), C.ulong(Culong(flags)), (*C.int)(&err)))
   if int(err) != 0 {
-    return ccodes_grib_iterator, errors.New(Cgrib_get_error_message(int(err)))
+    return ccodes_iterator, errors.New(Cgrib_get_error_message(int(err)))
   }
-
-  return ccodes_grib_iterator, nil
+  return ccodes_iterator, nil
 }
 
-func Ccodes_grib_iterator_next(kiter Ccodes_grib_iterator) (latitude float64, longitude float64, value float64, err error) {
+func Ccodes_grib_iterator_next(kiter Ccodes_iterator) (latitude float64, longitude float64, value float64, err error) {
 	res := int(C.codes_grib_iterator_next(
 		(*C.codes_iterator)(kiter),
 		(*C.double)(unsafe.Pointer(&latitude)),
@@ -31,14 +29,13 @@ func Ccodes_grib_iterator_next(kiter Ccodes_grib_iterator) (latitude float64, lo
 	if res != 0 {
 		return 0, 0, 0, errors.New(Cgrib_get_error_message(int(res)))
 	}
-
 	return latitude, longitude, value, nil
 }
 
-func Ccodes_grib_iterator_delete(kiter Ccodes_grib_iterator) error {
-	err := C.codes_grib_iterator_delete((*C.codes_iterator)(kiter))
-	if err != 0 {
-		return errors.New(Cgrib_get_error_message(int(err)))
+func Ccodes_grib_iterator_delete(kiter Ccodes_iterator) error {
+	res := C.codes_grib_iterator_delete((*C.codes_iterator)(kiter))
+	if res != 0 {
+		return errors.New(Cgrib_get_error_message(int(res)))
 	}
 	return nil
 }
