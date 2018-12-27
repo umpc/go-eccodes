9740d27cf1788b9d19e3fa09d8a56eff09ce8614
Justin Lowery <umpc@users.noreply.github.com>
2018-12-27 15:36:16 -0500

    Update eccodes-iterator.go

--- native/eccodes-iterator.go
+++ native/eccodes-iterator.go
@@ -11,10 +11,12 @@ import (
 )
 
 func Ccodes_grib_iterator_new(handle Ccodes_handle, flags int) (Ccodes_iterator, error) {
-	res := C.int(0)
-	ccodes_iterator := unsafe.Pointer(C.codes_grib_iterator_new((*C.codes_handle)(handle), C.ulong(Culong(flags)), (*C.int)(&res)))
-	if int(res) != 0 {
-		return ccodes_iterator, errors.New(Cgrib_get_error_message(int(res)))
+	cResult := C.int(0)
+	res := int(cResult)
+
+	ccodes_iterator := unsafe.Pointer(C.codes_grib_iterator_new((*C.codes_handle)(handle), C.ulong(Culong(flags)), (*C.int)(&cResult)))
+	if res != 0 {
+		return ccodes_iterator, errors.New(Cgrib_get_error_message(res))
 	}
 	return ccodes_iterator, nil
 }
@@ -27,15 +29,15 @@ func Ccodes_grib_iterator_next(kiter Ccodes_iterator) (latitude float64, longitu
 		(*C.double)(unsafe.Pointer(&value)),
 	))
 	if res != 0 {
-		return 0, 0, 0, errors.New(Cgrib_get_error_message(int(res)))
+		return 0, 0, 0, errors.New(Cgrib_get_error_message(res))
 	}
 	return latitude, longitude, value, nil
 }
 
 func Ccodes_grib_iterator_delete(kiter Ccodes_iterator) error {
-	res := C.codes_grib_iterator_delete((*C.codes_iterator)(kiter))
+	res := int(C.codes_grib_iterator_delete((*C.codes_iterator)(kiter)))
 	if res != 0 {
-		return errors.New(Cgrib_get_error_message(int(res)))
+		return errors.New(Cgrib_get_error_message(res))
 	}
 	return nil
 }
